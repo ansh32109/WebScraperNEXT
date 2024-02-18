7e99cb37678eedbf500232eff901bdf194b6582f
Ainesh Sinha <studentainesh@gmail.com>
2024-02-18 14:45:39 +0530

    Made further updates

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,8 +1,10 @@
 "use server"
 
+import { revalidatePath } from "next/cache";
 import Product from "../models/product.model";
 import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
+import { getAveragePrice, getHighestPrice, getLowestPrice } from "../scraper/utils";
 
 export async function scrapeAndStoreProduct(productURL: string) {
     if(!productURL){
@@ -18,7 +20,31 @@ export async function scrapeAndStoreProduct(productURL: string) {
 
         let product = scrapedProduct;
 
-        const existingProduct = await Product.findOne()
+        const existingProduct = await Product.findOne({ url: scrapedProduct.url});
+
+        if(existingProduct){
+            const updatedPriceHistory: any = [
+                ...existingProduct.priceHistory,
+                {price: scrapedProduct.currentPrice},
+            ];
+
+            product = {
+                ...scrapedProduct,
+                priceHistory: updatedPriceHistory,
+                lowestPrice: getLowestPrice(updatedPriceHistory),
+                highestPrice: getHighestPrice(updatedPriceHistory),
+                averagePrice: getAveragePrice(updatedPriceHistory),
+            }
+        }
+
+        const newProduct = await Product.findOneAndUpdate(
+            {url: scrapedProduct.url },
+            product,
+            {upsert: true, new: true},
+        );
+
+        revalidatePath(`/product/${newProduct.id}`);
+
     } catch (error: any) {
         throw new Error(`Failed to update/create the product: ${error.message}`)
     }
